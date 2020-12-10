3f8aef69e013b349b4a505ad4ab894a08e14bf30
Will Carter <will.carter@fergusllc.com>
2020-12-09 20:58:40 -0700

    added nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom"
+import { Link } from 'react-router-dom'
 import { Provider } from "react-redux"
 import PieContainer from "./containers/PieContainer"
 import PieForm from "./components/PieForm"
 import store from "./store"
-import PieGalleryContainer from "./containers/PieGalleryContainer"
+import PieGallery from "./components/PieGallery"
 import LoginForm from './components/LoginForm'
 import SignupForm from './components/SignupForm'
 import Home from './components/Home'
 import MyPies from "./components/MyPies"
-import PrivateRoute from './components/PrivateRoute';
+import PrivateRoute from './components/PrivateRoute'
+
 
 const App = () => {
   return (
     <Provider store={ store }>
       <Router>
+        <nav>
+          <div><Link to={"/home"}>Home</Link></div>
+          <div><Link to={"/login"}>Login</Link></div>
+          <div><Link to={"/logout"}>Logout</Link></div>
+          <div><Link to={"/signup"}>Signup</Link></div>
+          <div><Link to={"/pies"}>Pies</Link></div>
+        </nav>
         <Switch>
-          <Route exact path="/" component={ Home } />
+          <Route exact path="/" component={ PieGallery } />
           <Route exact path="/login" component={ LoginForm } />
           <Route exact path="/signup" component={ SignupForm } />
-          <Route exact path="/pies" exact render={(props) => (<PieGalleryContainer {...props} />)} />
           <PrivateRoute exact path="/mypies" component={ MyPies } />
           <PrivateRoute exact path="/pies/new" render={(props) => (<PieForm {...props} />)} />
           <Route exact path="/pies/:id" render={(props) => (<PieContainer {...props} id={ parseInt(props.match.params.id) }/>)} />
