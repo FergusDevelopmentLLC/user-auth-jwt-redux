da31a9a1e05aae5073638ae546511c2529740fa7
Will Carter <will.carter@fergusllc.com>
2020-12-09 21:28:14 -0700

    refactor

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
 
 const initialState = {}
 
@@ -15,4 +15,4 @@ const store = createStore(
   )
 );
 
-export default store;
+export default store
