a617a335baca94c8a8e8e542f546d5247f5a5e2d
Will Carter <will.carter@fergusllc.com>
2020-12-12 00:48:15 -0700

    refactor

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { signup } from '../actions/userActions'
 import { useHistory } from "react-router";
