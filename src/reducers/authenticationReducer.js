28196844180ac9c6c76ced2f4b411b64b3f9a659
Will Carter <will.carter@fergusllc.com>
2020-12-11 19:40:40 -0700

    removed REFRESH_USER cases, made sure loggedIn setting is correct

--- src/reducers/authenticationReducer.js
+++ src/reducers/authenticationReducer.js
@@ -1,4 +1,4 @@
-import { REFRESH_USER_REQUEST, REFRESH_USER_SUCCESS, REFRESH_USER_FAILURE, SIGNUP_REQUEST, SIGNUP_SUCCESS, SIGNUP_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../actions/types';
+import { SIGNUP_REQUEST, SIGNUP_SUCCESS, SIGNUP_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../actions/types';
 
 const initialState = {
   loggedIn: false,
@@ -9,45 +9,37 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_REQUEST:
       return {
-        loggingIn: true,
         user: action.user
-      };
+      }
     case LOGIN_SUCCESS:
       return {
         loggedIn: true,
         user: action.user
-      };
+      }
     case LOGIN_FAILURE:
       return {
+        loggedIn: false,
         errorMessage: action.error
-      };
+      }
     case LOGOUT:
-      return {};
+      return {
+        loggedIn: false,
+        user: {}
+      }
     case SIGNUP_REQUEST:
       return {
-        loggingIn: true,
         user: action.user
-      };
+      }
     case SIGNUP_SUCCESS:
       return {
         loggedIn: true,
         user: action.user
-      };
+      }
     case SIGNUP_FAILURE:
       return {
+        loggedIn: false,
         errorMessage: action.error
-      };
-    case REFRESH_USER_REQUEST:
-      return state
-    case REFRESH_USER_SUCCESS:
-      return {
-        loggedIn: true,
-        user: action.user
-      };
-    case REFRESH_USER_FAILURE:
-      return {
-        errorMessage: action.error
-      };
+      }
     default:
       return state
   }
